c62acb5be2e087b37375b40133c7ccc5e0271cbf
Carlos Eduardo Souza de Oliveira <122989955+Carlos-Eduardo-Oliveira@users.noreply.github.com>
2023-05-17 22:56:10 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+
+const botaoAterarTema = document.getElementById("botao-alterar-tema");
+
+const body = document.querySelector("body");
+
+const imagemBotaoTrocaDeTema =document.querySelector(".imagem-botao");
+
+botaoAterarTema.addEventListener("click", () => {
+    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+
+    if (modoEscuroEstaAtivo) {
+        body.classList.remove("modo-escuro");
+
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png");
+
+    }   
+    
+    else {
+        body.classList.add("modo-escuro");
+
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png");
+    }
+});
